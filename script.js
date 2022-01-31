7fd57007267aafd9d09689e4a66e9d668cd71d32
Vaishnavi Sharma <vaishnavisharma137@gmail.com>
2022-01-31 11:52:05 +0530

    Added Clear All button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const lookupbtn = document.querySelector(".btn");
-const ipdisplay = document.querySelector(".ip-display");
-const locdisplay = document.querySelector(".loc-display");
-const geodisplay = document.querySelector(".geo-display");
-const loader = document.querySelector(".loader-container");
-const details = document.querySelector(".details");
-const btn = document.querySelector(".btn");
+let lookupbtn = document.querySelector(".btn");
+let ipdisplay = document.querySelector(".ip-display");
+let locdisplay = document.querySelector(".loc-display");
+let geodisplay = document.querySelector(".geo-display");
+let loader = document.querySelector(".loader-container");
+let details = document.querySelector(".details");
+let btn = document.querySelector(".btn");
+let clear = document.querySelector(".clear");
 
 lookupbtn.addEventListener("click", () => {
 
@@ -20,9 +21,16 @@ lookupbtn.addEventListener("click", () => {
         locdisplay.textContent = `Location : ${response.data.city}, ${response.data.region}, ${response.data.country_name}`;
         geodisplay.textContent = `Latitude : ${response.data.latitude}, Longitude : ${response.data.longitude}`;
 
-        btn.textContent = `Clear`;
-
     }); 
 });
+
+clear.addEventListener('click', () => {
+        loader.style.display = "none";
+        ipdisplay.textContent = `IP Address`;
+        locdisplay.textContent = `Location`;
+        geodisplay.textContent = `GeoLocation`;
+})
+
+
 
 
